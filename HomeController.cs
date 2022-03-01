eeff9021de4c77931ceb2104113069d4cfc69c22
Juanita Francis <100166085+jfrancis18@users.noreply.github.com>
2022-03-01 16:05:22 -0600

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,58 @@
+﻿using PriceQuotation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceQuotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            ViewBag.PQ = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculatePriceQuotation();
+                ViewBag.PQ = model.CalculateDiscountAmount();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+                ViewBag.PQ = 0;
+            }
+            return View(model);
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PriceQuotationModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotation.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter a subtotal.")]
+        [Range(1, 5000, ErrorMessage = "Subtotal amount must be between 1 and 5000.")]
+        public decimal? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percent.")]
+        [Range(0, 100, ErrorMessage = "Discount percent must be between 0  and 100")]
+        public decimal? DiscountPercent { get; set; }
+
+
+        public decimal? CalculateDiscountAmount()
+        {
+            decimal? DiscountAmount = (DiscountPercent / 100) * Subtotal;
+            for (int i = 0; i < Subtotal; i++)
+            {
+                DiscountAmount = (DiscountPercent / 100) * Subtotal;
+            }
+            return DiscountAmount;
+        }
+
+        public decimal? CalculatePriceQuotation()
+        {
+            decimal? DiscountAmount = (DiscountPercent / 100) * Subtotal;
+            decimal? PriceQuotation = 0;
+            for (int i = 0; i < Subtotal; i++)
+            {
+                PriceQuotation = (Subtotal - DiscountAmount);
+            }
+            return PriceQuotation;
+        }
+    }
+}
